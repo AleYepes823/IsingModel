b9bd023c155d1848fdad283b2967e493535bcebe
AleYepes823 <ayepesp@unal.edu.co>
2021-12-27 12:02:48 -0500

    modelo actualizado para solo un salto por iteracion

--- SimpleIsingModel.py
+++ SimpleIsingModel.py
@@ -16,22 +16,19 @@ def GenerateInitialState(N,M):
     State = 2*np.random.randint(2, size=(N,M))-1
     return State
 
-def Evolve(State,J,beta):
+def Evolve(State,J,beta,a,b,number):
     N,M = np.shape(State)
     NewState = State
-    for a in range(N):
-        for b in range(M):
-            number = NumberedState[a][b]
-            Energy = 0
-            for pos in [(-1,0),(0,-1),(1,0),(0,1)]:
-                if not ((a+pos[0] ==-1 or a+pos[0] ==N) or (b+pos[1] ==-1 or b+pos[1] ==M)):
-                    numbersum = NumberedState[a+pos[0]][b+pos[1]]
-                    Energy -= J[number][numbersum]*NewState[a,b]*NewState[a+pos[0]][b+pos[1]]
-            print(Energy)
-            if Energy < 0:
-                NewState[a][b] *= -1
-            elif np.random.rand() < np.exp(-Energy*beta):
-                    NewState[a][b] *= -1
+    Energy = 0
+    for pos in [(-1,0),(0,-1),(1,0),(0,1)]:
+        if not ((a+pos[0] ==-1 or a+pos[0] ==N) or (b+pos[1] ==-1 or b+pos[1] ==M)):
+            numbersum = NumberedState[a+pos[0],b+pos[1]]
+            Energy -= J[number,numbersum]*NewState[a,b]*NewState[a+pos[0],b+pos[1]]/np.sqrt(M*N)
+    print(Energy)
+    if Energy < 0:
+        NewState[a,b] *= -1
+    if Energy >= 0 and np.random.rand() < np.exp(-Energy*beta):
+        NewState[a,b] *= -1
     return NewState
 
 
@@ -44,15 +41,17 @@ J = np.ones(shape = (N*M,N*M))*2
 J -= np.diag(np.diag(J))
 ax.imshow(State0,cmap = cmap)
 plt.draw()
-plt.pause(0.1)
+plt.pause(0.01)
 NumberedState = np.arange(N*M).reshape(N,M)
+Iteracion = 50000
 State = State0.copy()
-fig.suptitle('Time = 0')
-for m in range(1,200):
+for i in range(Iteracion):
+    particle = np.random.randint(M*N)
+    a,b = np.where(NumberedState == particle)
     ax.clear()
-    fig.suptitle(f'Time = {m}')
-    State = Evolve(State,J,1/300.0)
+    fig.suptitle(f'Time = {i}')
+    State = Evolve(State,J,0,a,b,particle)
     ax.imshow(State,cmap = cmap)
     plt.draw()
-    plt.pause(0.1)
+    plt.pause(0.01)
 plt.close()
